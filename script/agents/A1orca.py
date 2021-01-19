bf1a28c296ce041665a68698d38362a5cbbd8b9b
SeunghyunLim <shl0216@dgist.ac.kr>
2021-01-19 16:46:06 +0900

    update for agents with ORCA

---
+++ script/agents/A1orca.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+import rvo2
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import rospy
+import time
+
+import A1easyGo as easyGo
+
+import sys
+sys.path.append("..")
+import A1scan2obs as pc2obs
+
+rospy.init_node('A1_mvs', anonymous=False)
+
+SIMUL_HZ = 10.0
+
+sim = rvo2.PyRVOSimulator(1/SIMUL_HZ, 15.0, 10, 5.0, 2.0, 0.15, 3.0)
+
+# parameter description (implementations in RVOSimulator.cpp)
+# timeStep:
+# neighborDist : max dist (center to center) to other agents
+# maxNeighbors: max num of other agengts the agent takes into account in navigation
+# timeHorizon: min amount of time to compute to be safe with respect to agents
+# timeHorizonObst: min amount of time to compute to be safe with respect to obstacles
+# radius: radius of agent
+# maxSpeed: max speed of te agent
+# velocity (Vector2 &) : current velocity of the agent (setAgentPrefVelocity())
+
+COL = 10.0
+ROW = 10.0
+voxel_size = 0.3
+size = voxel_size/2
+
+#ROBOT MOVE
+SPEED = 20 # 14
+ROTATE_SPEED = 15 # 25
+
+def GoEasy(direc):
+	if direc == 4: # Backward
+		easyGo.mvStraight(- SPEED, -1)
+	elif direc == 0 or direc == 1: # Go straight
+		easyGo.mvStraight(SPEED, -1)
+	elif direc == 2: # turn left
+		easyGo.mvRotate(ROTATE_SPEED, -1, False)
+	elif direc == 3: # turn right
+		easyGo.mvRotate(ROTATE_SPEED, -1, True)
+	elif direc == 5: # stop
+		easyGo.stop()
+
+#MIN_OBS_SIZE = 0.6 / 2
+#MAX_OBS_SIZE = 1.4 / 2
+
+
+# make random square object
+'''
+obs_center_size = [(random.uniform(-COL, COL), random.uniform(0, ROW), random.uniform(MIN_OBS_SIZE, MAX_OBS_SIZE)) for i in range(15)]
+# osb_position must be convex in counter clock wise order
+obs_position_list = [[(x-size, y-size),(x+size, y-size), (x+size, y+size), (x-size, y+size)] for x,y,size in obs_center_size]
+obs = [sim.addObstacle(obs_position) for obs_position in obs_position_list]
+'''
+
+# single obstacle for test
+# obs_position_list = [[(6.1,6.1), (4.1, 6.1), (4.1, 4.1)]]
+#o1 = sim.addObstacle([(6.1,6.1), (4.1, 6.1), (4.1, 4.1)])
+# obs_position_list = np.array(obs_position_list)
+sim.processObstacles()
+
+agents_position =[(0,0)]
+
+agents = [sim.addAgent(position, 15.0, 10, 5.0, 2.0, 0.15, 3.0, (0.0,3.0)) for position in agents_position]
+agents_velocity = [(0.0, 0.5)]
+for agent, velocity in zip(agents, agents_velocity):
+	sim.setAgentPrefVelocity(agent, velocity)
+
+print('Simulation has %i agents and %i obstacle vertices in it.' %
+	(sim.getNumAgents(), sim.getNumObstacleVertices()))
+
+print("init pc2obs")
+pc2obs.pc2obs_init()
+
+print('Running simulation')
+
+pc2obs_time = 0.0
+lpp_time = 0.0
+for step in range(100):
+	t1 = time.time()
+	samples = pc2obs.pc2obs(voxel_size = voxel_size)
+	t2 = time.time()
+	# print(samples)
+	if type(samples) == type(False):
+		continue
+
+
+	sim.clearObstacle()
+	obs_position_list = [[(x-size, y-size),(x+size, y-size), (x+size, y+size), (x-size, y+size)] for x,y in samples]
+	obs = [sim.addObstacle(obs_position) for obs_position in obs_position_list]
+	sim.processObstacles()
+
+	#for agent, velocity in zip(agents, agents_velocity):
+	#    sim.setAgentPrefVelocity(agent, velocity)
+
+	# always set agent position as origin
+	sim.setAgentPosition(0, (0,0))
+	positions = [sim.getAgentPosition(agent) for agent in agents]
+
+	sim.doStep()
+	#positions = [sim.getAgentPosition(agent) for agent in agents]
+
+	#print('step=%2i  t=%.3f  %s' % (step, sim.getGlobalTime(), '  '.join(positions)))
+	positions = np.array(positions)
+	obs_position_list = np.array(obs_position_list)
+	velocity = [sim.getAgentVelocity(agent) for agent in agents]
+	print("Agent velocity: {}, {}".format(velocity[0][0], velocity[0][1]))
+	if velocity[0][0] < 0:
+		direc = 2 # turn left
+	elif velocity[0][0] > 0:
+		direc = 3 # turn right
+	elif velocity[0][1] > 0:
+		direc = 1 # go straight
+	elif velocity[0][1] < 0:
+		direc = 4 # backward
+	else:
+		direc = 5 # stop
+	GoEasy(direc)
+	t3 = time.time()
+
+	pc2obs_time += t2-t1
+	lpp_time += t3-t2
+
+	# print("pc2obs took: {} sec".format(t2-t1))
+	# print("OCRA took: {} sec".format(t3-t2))
+	# print("Average took: {} sec, {} sec".format(pc2obs_time/step, lpp_time/step))
+	#
+	# plt.arrow(positions[0][0], positions[0][1], velocity[0][0], velocity[0][1], width=0.05)
+	# for obs_position in obs_position_list:
+	# 	plt.plot(np.hstack([obs_position[:,0],obs_position[0][0]]), np.hstack([obs_position[:,1],obs_position[0][1]]))
+	# plt.scatter(positions[:,0], positions[:,1], label='agents')
+	# if len(samples) != 0:
+	# 	plt.scatter(samples[:,0], samples[:,1], label='samples')
+	# plt.legend()
+	# plt.title("Trajectories of the agnets")
+	# plt.xlabel("x (m)")
+	# plt.ylabel("y (m)")
+	# plt.xlim(-5,5)
+	# plt.ylim(-2,8)
+	# plt.pause(0.001)
+	# plt.cla()
+	print("{:.6f} sec simulated".format(step/SIMUL_HZ))
+	time.sleep(0.1)
+
+easyGo.stop()
+rospy.signal_shutdown("esc")
+sys.exit(1)
---
+++ script/agents/A2scan2obs.py
@@ -0,0 +1,219 @@
+# ECSC on the opencv image to quit
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import math
+import time
+import sys
+
+import rospy
+import sensor_msgs.point_cloud2 as pc2
+from sensor_msgs.msg import PointCloud2, CompressedImage, LaserScan
+from geometry_msgs.msg import Twist
+
+import easyGo
+from std_msgs.msg import String
+from cv_bridge import CvBridge, CvBridgeError
+import threading
+from time import sleep
+import csv
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--control', action='store_true')
+parser.add_argument('--plot', action='store_true')
+parser.add_argument('--csv', action='store_true')
+args = parser.parse_args()
+
+global depth_scale, ROW, COL
+global currentStatus
+
+if args.csv:
+	CSV_NAME = "office_01"
+
+	f= open(CSV_NAME+'.csv','w')
+	wr = csv.writer(f)
+	wr.writerow(["time", \
+				"linear_x", "angular_z", \
+				"deadends"])
+
+#size of images
+COL= 480
+ROW = 640
+
+#ROBOT MOVE
+SPEED = 15
+ROTATE_SPEED = 25
+
+currentStatus = ""
+font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+fontScale = 1.5
+yellow = (0, 255, 255)
+handle_easy = True
+points_raw = 0
+cmd_vel = 0
+
+t = time.time()
+
+#Topview image. src, dst are numpy array.
+#########################Move LAVACON TO EACH EDGES AND TRY AGAIN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def Topview(src):
+	global WARP_PARAM, ROW, COL
+	# col=720, row=1280
+	col, row = src.shape[0], src.shape[1]
+
+	corners = np.float32([[row*WARP_PARAM/2, 0], [row*(1-WARP_PARAM/2), 0], [0, col], [row, col]])
+	warp_corners = np.float32([[0, 0], [ROW, 0], [0, COL], [ROW, COL]])
+
+	trans_matrix = cv2.getPerspectiveTransform(corners, warp_corners)
+
+	dst = cv2.warpPerspective(src, trans_matrix, (ROW, COL))
+	return dst
+
+
+def preGroundSeg(depth_image, color_image):
+	global ROW, COL, GRN_ROI
+	# FIXME: don't do ZOOM_PARAM in GAZEBO
+	# ROI image
+	depth_image = depth_image[GRN_ROI:COL, 0:ROW]
+	color_image = color_image[GRN_ROI:COL, 0:ROW]
+
+	# Topview image
+	depth_image2 = Topview(depth_image)
+	color_image2 = Topview(color_image)
+	return depth_image2, color_image2
+
+
+def GroundSeg(depth_image, color_image, stride=80):
+	global ROW
+	virtual_lane_available = []
+	for i in range(stride, ROW, stride):
+		if args.plot and i == ROW/2:
+			temp_image, dead_end = verticalGround(depth_image, color_image, i, plot=True)
+		else:
+			temp_image, dead_end = verticalGround(depth_image, color_image, i, plot=False)
+		virtual_lane_available.append(dead_end)
+	return temp_image, virtual_lane_available
+
+def GoEasy(direc):
+	if direc == 4:
+		easyGo.mvStraight(- SPEED, -1)
+	elif direc == 0 or direc == 1:
+		#print("COME HERE")
+		easyGo.mvStraight(SPEED, -1)
+	elif direc == 2:
+		#print("COME HERE2")
+		easyGo.mvRotate(ROTATE_SPEED, -1, False)
+	elif direc == 3:
+		easyGo.mvRotate(ROTATE_SPEED, -1, True)
+
+def points_callback(data):
+	global points_raw
+	temp = []
+	for idx, elem in enumerate(data.ranges):
+		if elem > 5.0:
+			continue
+		_y = math.cos(math.pi/180*idx)*elem
+		_x = math.sin(math.pi/180*idx)*elem
+		temp.append([_x, _y])
+		#print(temp)
+	points_raw = temp
+
+def cmd_callback(data):
+	global cmd_vel
+	cmd_vel = data
+
+def listener():
+	rospy.Subscriber("/tb3_1/scan", LaserScan, points_callback)
+	# spin() simply keeps python from exiting until this node is stopped
+	rospy.spin()
+
+direc = 0
+
+def pc2obs_init():
+	# Configure depth and color streams
+	global bridge
+
+	bridge = CvBridge()
+
+	realsense_listener = threading.Thread(target=listener)
+	realsense_listener.start()
+
+
+def pc2obs(voxel_size = 0.3, plot=False):
+	global points_raw, bridge, currentStatus, handle_easy
+	#print(points_raw)
+	if type(points_raw) == type(0):
+		print("NOT CONNECTED")
+		sleep(0.1)
+		return False
+
+	t1 = time.time()
+	points = np.array(list(points_raw), dtype=np.float32)
+	if len(points) == 0:
+		return False
+
+	t2 = time.time()
+	#print("length pre-processed points: {}".format(len(points)))
+	np_vox = np.ceil((np.max(points, axis=0) - np.min(points, axis=0)) / voxel_size)
+	non_empty_voxel_keys, inverse, nb_pts_per_voxel = np.unique(((points - np.min(points, axis=0)) // voxel_size).astype(int), axis=0, return_inverse=True, return_counts=True)
+	idx_pts_sorted = np.argsort(inverse)
+	voxel_grid = {}
+	grid_barycenter, grid_candidate_center = [], []
+	last_seen = int(0)
+
+	for idx, vox in enumerate(non_empty_voxel_keys):
+		voxel_grid[tuple(vox)] = points[idx_pts_sorted[int(last_seen):int(last_seen + nb_pts_per_voxel[idx])]]
+		grid_barycenter.append(np.mean(voxel_grid[tuple(vox)], axis=0))
+		grid_candidate_center.append(voxel_grid[tuple(vox)][np.linalg.norm(voxel_grid[tuple(vox)] - np.mean(voxel_grid[tuple(vox)], axis=0), axis=1).argmin()])
+		last_seen += nb_pts_per_voxel[idx]
+	points = np.array(filter(lambda x: x[0] != 0, list(grid_candidate_center)))
+
+	t3 = time.time()
+	points_layer = []
+	for i, p in enumerate(points):
+		#if -p[1] > 0.1 and -p[1] < 0.6:
+		#if p[2] > 0.1 and p[2] < 0.6:
+		#if abs(-p[1] - 0.3) < 0.06: # points are at 0.2m higher height than depth camera
+			# forward:x  . right:y,  up:z
+		points_layer.append([-p[0], p[1]])
+			#points_layer.append([p[0], p[2], -p[1]])
+	samples = np.array(points_layer)
+
+	if plot:
+		print("time took")
+		print(t2-t1)
+		print(t3-t2)
+		print(time.time() - t3)
+
+		plt.scatter(points[:,0], points[:,2], label='voxel grid filtering')
+		plt.scatter(samples[:,0], samples[:,1], label='height filtering')
+		plt.xlim(-1.5,1.5)
+		plt.ylim(0,6)
+		plt.legend()
+		plt.title("Top view points after filter processing")
+		plt.xlabel("x (m)")
+		plt.ylabel("y (m)")
+		plt.pause(0.05)
+		plt.cla()
+		plt.clf()
+	#cv2.imshow('RealSense_depth', depth_image)
+	if cv2.waitKey(1) == 27: #esc
+		easyGo.stop()
+		rospy.signal_shutdown("esc")
+		if args.csv:
+			f.close()
+		sys.exit(1)
+
+	return samples
+
+if __name__ == "__main__":
+	rospy.init_node('robot_mvs', anonymous=False)
+	pc2obs_init()
+	while True:
+		samples = pc2obs(voxel_size = 0.3, plot=True)
+		# print(samples)
+	f.close()
+	exit()
