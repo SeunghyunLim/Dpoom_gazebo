eb7fd188507cc8432542b9a45b53fc0f0127526a
SeunghyunLim <shl0216@dgist.ac.kr>
2021-02-02 17:33:59 +0900

    update initialization for agents

--- script/agents/utils.py
+++ script/agents/utils.py
@@ -1,9 +1,16 @@
 import math
 
 def qut2eu(x, y, z, w):
-	t3 = 2.0 * (w*z + x*y)
-	t4 = 1.0 - 2.0*(y*y + z*z)
-	yaw = math.atan2(t3, t4)
-	return yaw
+    t3 = 2.0 * (w*z + x*y)
+    t4 = 1.0 - 2.0*(y*y + z*z)
+    yaw = math.atan2(t3, t4)
+    return yaw
 
-print(qut2eu(-0.000189, 0.001578, 0.118296, 0.992977))
+def eu2qut(yaw, pitch = 0, roll = 0):
+        qx = math.sin(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) - math.cos(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
+        qy = math.cos(roll/2) * math.sin(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.cos(pitch/2) * math.sin(yaw/2)
+        qz = math.cos(roll/2) * math.cos(pitch/2) * math.sin(yaw/2) - math.sin(roll/2) * math.sin(pitch/2) * math.cos(yaw/2)
+        qw = math.cos(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
+        return [qx, qy, qz, qw]
+
+#print(eu2qut(yaw=-1.57))
